dd72504d8f19f452d85543b327b4ef54231f63a3
sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2023-01-14 20:53:31 +0530

    Update isobuildntsc.sh

--- isobuildntsc.sh
+++ isobuildntsc.sh
@@ -1,3 +1,30 @@
 #This will build isos, WIP.
+mkdir sm64iso
+#cp build/*_ps2/*.elf sm64iso
 
-#cp build/*_ps2/*.elf 
+
+# if ntsc, copy ntsc system.cnf
+
+#if pal, copy pal system.cnf
+
+echo -n "What is the rom region $Region ? "
+
+case $Region in
+
+  NTSC)
+    echo -n "Executing for NTSC"
+    #cp isoneeds/ntsc/*.cnf sm64iso
+    ;;
+
+  Romania | Moldova)
+    echo -n "Romanian"
+    ;;
+
+  Italy | "San Marino" | Switzerland | "Vatican City")
+    echo -n "Italian"
+    ;;
+
+  *)
+    echo -n "unknown"
+    ;;
+esac
